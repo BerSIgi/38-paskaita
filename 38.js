086f8777cc4c9a6f738adc9e4dd866a103ef056c
Sigita Bernotiene <BerSIgi>
2022-02-05 11:42:37 +0200

    kitas sprendimas su regex

---
+++ 38.js
@@ -0,0 +1,8 @@
+const vardas = 'Sigita Bernotiene';
+const raidziuSkaicius = vardas.length;
+const fullName = vardas.split(' ');
+console.log(fullName);
+const vardasMazosiomis = vardas.toLowerCase();
+const vardasDidziosiomis = vardas.toUpperCase();
+const apkeistaRaide = vardas.replace('i', 'I')
+console.log(apkeistaRaide);
